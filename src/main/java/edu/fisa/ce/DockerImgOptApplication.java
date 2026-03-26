0edb1364841f9c08f81bb4d647774db9b6bc920a
Soooonnn <ksj386502@gmail.com>
2026-03-26 15:44:45 +0900

    자동화 테스트1

---
+++ src/main/java/edu/fisa/ce/DockerImgOptApplication.java
@@ -0,0 +1,13 @@
+package edu.fisa.ce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerImgOptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerImgOptApplication.class, args);
+	}
+
+}
---
+++ src/main/java/edu/fisa/ce/controller.java
@@ -0,0 +1,15 @@
+package edu.fisa.ce;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+	
+	@GetMapping("ce1")
+	public String getDate() {
+		System.out.println("수정 후!");
+		return "응답 데이터 수정 후!! 자동 배포";
+	}
+
+}
