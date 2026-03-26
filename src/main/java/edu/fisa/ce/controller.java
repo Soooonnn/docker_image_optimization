79a594398dbdf494614adeb4c4ef325fb45f1036
Soooonnn <ksj386502@gmail.com>
2026-03-26 16:37:12 +0900

    테스트3

--- src/main/java/edu/fisa/ce/controller.java
+++ src/main/java/edu/fisa/ce/controller.java
@@ -9,7 +9,7 @@ public class controller {
 	@GetMapping("ce1")
 	public String getDate() {
 		System.out.println("수정 후!");
-		return "응답 데이터 수정 후!! 자동 배포 테스트1";
+		return "응답 데이터 수정 후!! 자동 배포 테스트2";
 	}
 
 }
